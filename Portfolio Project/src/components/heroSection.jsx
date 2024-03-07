7f81169d5ad92a53ec9935ee6ff4632954e2480c
tewodrosgebreselassie <tedyfiseha3@gmail.com>
2024-03-07 14:19:41 +0300

    adding assets

--- Portfolio Project/src/components/heroSection.jsx
+++ Portfolio Project/src/components/heroSection.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Typewriter from "typewriter-effect";
+import { Bio } from '../data/fixedData';
 import {
   Button,
   Typography,
@@ -17,29 +19,34 @@ export default function HeroSection() {
               color="blue-gray"
               className="mx-auto my-6 w-full leading-snug  !text-2xl lg:max-w-3xl lg:!text-5xl"
             >
-              Get ready to experience a new level of{" "}
-              <span className="text-green-500 leading-snug ">
-                performance
-              </span>{" "}
-              and{" "}
-              <span className="leading-snug text-green-500">
-                functionality
-              </span>
-              .
+              Hi, I'm {Bio.name}<br />
+            </Typography>
+            <Typography variant="h1"
+              color="blue-gray"
+              className="mx-auto my-6 w-full leading-snug  !text-2xl lg:max-w-3xl lg:!text-5xl">
+                I am
+                  <span className="leading-snug text-blue-500">
+                    <Typewriter 
+                      options={{ 
+                        strings: Bio.roles, 
+                        autoStart: true, 
+                        loop: true,
+                      }} 
+                    />
+                  </span>
             </Typography>
             <Typography
               variant="lead"
               className="mx-auto w-full !text-gray-500 lg:text-lg text-base"
             >
-              The time is now for it to be okay to be great. For being a bright
-              color. For standing out.
+              {Bio.description}
             </Typography>
             <div className="mt-8 grid w-full place-items-start md:justify-center">
               <div className="mb-2 flex w-full flex-col gap-4 md:flex-row">
-                <a href="#buttons-with-link" target="_blank" rel="noreferrer">
+                <a href={Bio.resume} target="_blank" rel="noreferrer">
                   <Button color="green" className="w-full px-4 md:w-[12rem]">View Resume</Button>
                 </a>
-                <a href="#buttons-with-link" target="_blank" rel="noreferrer">
+                <a href={Bio.github} target="_blank" rel="noreferrer">
                   <Button color="gray" className="w-full px-4 md:w-[12rem]">Visit GitHub Account</Button>
                 </a>
               </div>
---
+++ Portfolio Project/src/data/fixedData.jsx
@@ -0,0 +1,114 @@
+import react from "../images/react.png";
+import js from "../images/javaScript.png";
+import html from "../images/html.png";
+import bootstrap from "../images/bootstrap.png";
+import materialui from "../images/materialUi.png";
+import css from "../images/css.png";
+import tailwind from "../images/tailwind.png";
+import node from "../images/node.png";
+import mongoDB from "../images/mongoDB.png";
+import mySql from "../images/mySql.png";
+import vsCode from "../images/vsCode.png";
+import github from "../images/github.png";
+import git from "../images/git.png";
+import ps from "../images/ps.png";
+import figma from "../images/figma.png";
+
+export const Bio = {
+  name: "Junior Worku",
+  roles: [
+    "Front-end developer",
+    "Web Developer",
+    "Grahics Designer",
+    "Programmer",
+  ],
+  description:
+    "a versatile web developer who loves to create beautiful and functional websites for people who want to experience the full emersion of creativity.",
+  resume:
+    "https://drive.google.com/file/d/1fp_VDnXsfmdrWBn223QempNEUJHa_r83/view?usp=drive_link",
+  github: "https://github.com/juniorworku",
+};
+
+export const socialLinks = {
+  github: "https://github.com/juniorworku",
+  linkedin: "https://www.linkedin.com/in/junior-worku-9298b919b/",
+  telegram: "https://t.me/junior_worku",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: react,
+      },
+      {
+        name: "Javascript",
+        image: js,
+      },
+      {
+        name: "HTML5",
+        image: html,
+      },
+      {
+        name: "Bootstrap",
+        image: bootstrap,
+      },
+      {
+        name: "Material UI",
+        image: materialui,
+      },
+      {
+        name: "CSS3",
+        image: css,
+      },
+      {
+        name: "Tailwind CSS",
+        image: tailwind,
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node.Js",
+        image: node,
+      },
+      {
+        name: "MongoDB",
+        image: mongoDB,
+      },
+      {
+        name: "MySQL",
+        image: mySql,
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Vs Code",
+        image: vsCode,
+      },
+      {
+        name: "GitHub",
+        image: github,
+      },
+      {
+        name: "Git",
+        image: git,
+      },
+      {
+        name: "Adobe Photoshop",
+        image: ps,
+      },
+      {
+        name: "Figma",
+        image: figma,
+      },
+    ],
+  },
+];
