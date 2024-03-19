9163090c0146ee5c8b9a81ab0d252fa16a4b62b5
juniorworku <juniorworku@gmail.com>
2024-03-19 10:02:03 +0300

    navbar bug fixed

--- Portfolio Project/src/components/navBar.jsx
+++ Portfolio Project/src/components/navBar.jsx
@@ -37,48 +37,6 @@ export default function NavBar() {
     }
     `;
 
- 
-  const navList = (
-    <ul className="mt-4 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <NavLink
-        to="about" 
-        smooth={true} 
-        duration={1500}
-        as="li"
-        className="flex items-center gap-x-2 p-1 font-medium"
-      >
-        <a href="#about" smooth={true} duration={1500} className="flex items-center" >
-         About Me
-        </a>
-       
-      </NavLink>
-      <NavLink
-        as="li"
-        className="flex items-center gap-x-2 p-1 font-medium"
-      >
-        <a href="#skills" smooth={true} duration={1500} className="flex items-center" >
-          Skills
-        </a>
-      </NavLink>
-      <NavLink
-        as="li"
-        className="flex items-center gap-x-2 p-1 font-medium"
-      >
-        <a href="#project" smooth={true} duration={1500} className="flex items-center" >
-        Projects
-        </a>
-      </NavLink>
-      <NavLink 
-        as="li"
-        className="flex items-center gap-x-2 p-1 font-medium"
-      >
-        <a href="#contact" smooth={true} duration={1500} className="flex items-center" >
-          Contact Me
-        </a>
-      </NavLink>
-    </ul>
-  );
- 
   return (
     <Navbar className="bg-light-green-100 mx-auto max-w-screen-xl px-4 py-2 lg:rounded-full lg:py-4">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
@@ -91,7 +49,50 @@ export default function NavBar() {
             </Typography>
           </div>
         </div>
-        <div className="hidden lg:block">{navList}</div>
+        <div className="hidden lg:block">
+          <ul className="mt-4 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            <li>
+              <NavLink
+                to="about" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                About Me
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="skills" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                Skills
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="project" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="contact" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                Contact Me
+              </NavLink>
+            </li>
+          </ul>
+        </div>
         <IconButton
           variant="text"
           className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -132,7 +133,49 @@ export default function NavBar() {
       </div>
       <MobileNav open={openNav}>
         <div className="container mx-auto">
-          {navList}
+          <ui>
+            <li>
+              <NavLink
+                to="about" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                About Me
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="skills" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                Skills
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="project" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to="contact" 
+                smooth={true} 
+                duration={1500}
+                className="flex items-center gap-x-2 p-1 font-medium"
+              >
+                Contact Me
+              </NavLink>
+            </li>
+            
+          </ui>
         </div>
       </MobileNav>
       
